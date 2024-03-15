46b431f4317e6d25f10b9c2df6ebc5837def254e
anurag <anurag.singh@crownstack.com>
2024-03-15 17:18:52 +0530

    Implemented the dynamic route grouping for web and api files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using:function () {
+            $wwwHost = env('WWW_HOSTNAME');
+            $apiHost = env('API_HOSTNAME');
+            Route::domain($wwwHost)->group(function () {
+                Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+            });
+            Route::domain($apiHost)->group(function () {
+                Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+            });
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
