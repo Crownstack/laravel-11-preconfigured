9820aae2d80a891596efe6bdd73ac06b54daa1bf
Abhishek Jha <abhishek.jha@crownstack.com>
2024-05-10 12:02:44 +0530

    fix:Comment removed from API route file

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,4 @@ use App\Exceptions\DivideByZeroException;
 
 Route::group(['prefix'=>'v1'], function(){
 
-
-    // Route::post('error-exceptions',function(){
-    //     $b ='new';
-        
-    //     // try {
-    //     //     $val = 100/0;
-    //         throw new DivideByZeroException("Divide by zero ");
-    //     // } catch ( $th) {
-    //     // }
-    //     // view('abcd');
-    //     // dd(DB::table('users')->get()->contacts());
-    //     dd('called');
-    // });
 });
