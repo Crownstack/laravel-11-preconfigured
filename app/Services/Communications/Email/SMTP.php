7ca4be903e5ae6fc1a18dac01cfd6e2265bb18c1
Abhishek Jha <abhishek.jha@crownstack.com>
2024-06-04 13:41:05 +0530

    feat:Email sending mechanism added

--- app/Services/Communications/Email/SMTP.php
+++ app/Services/Communications/Email/SMTP.php
@@ -2,42 +2,77 @@
 
 namespace App\Services\Communications\Email;
 
-use App\Services\Communications\BaseEmailChannel;
 use App\Jobs\EmailSenderJob;
 use Illuminate\Support\Facades\Mail;
 use App\Traits\ModelResponseTrait;
+use Illuminate\Contracts\Mail\Mailable;
+use App\Services\Communications\BaseMailable;
+use Exception;
+use App\Traits\EmailTrait;
 
-class SMTP extends BaseEmailChannel
+class SMTP  extends BaseMailable 
 {
     use ModelResponseTrait;
 
-    public $preparedMail;
+    public $data;
+    
+
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        if (!isset($data['to']) || !isset($data['subject']) || !isset($data['body'])) 
+        {
+            throw new Exception("To, Subject, and Body is required to send an email ");
+        }
+
+        $this->header = $data['header'] ?? '';
+        $this->body = $data['body'] ?? '';
+        $this->footer = $data['footer'] ?? '';
+        $this->subject = $data['subject'] ?? '';
+        
+        $data['cc'] = $data['cc'] ?? [];
+        $data['bcc'] = $data['bcc'] ?? [];      
+        $this->attachments = $data['attachments'] ?? [];      
+
+        $this->data = $data;
+    }
+    
+    /**
+     * Method to send email and used as the common method in all kind of service provider.
+     *
+     * @param  array $data
+     * 
+     * @return array $response
+     */
+    public function sendEmail()
+    {
+        return $this->triggerEmail();
     }
 
-    public function sendEmail($data)
-    {
-
-    }
-
-
+    
+    /**
+     * Method to dispatch a Job to send asynchronous emails.
+     *
+     * @return void
+     */
     public function sendAsyncEmail()
     {
         // EmailSenderJob::dispatch($receivers, $message);
     }
 
-
-    public function triggerEmail($data)
+    
+    /**
+     * Method to set the mail parameters and send email
+     *
+     * @return array $response
+     */
+    public function triggerEmail()
     {
-        Mail::to($data['to'])
-            ->cc($data['cc'])
-            ->bcc($data['bcc'])
-            ->send($data['preparedMail']);
+        $data = $this->data;
+
+        Mail::to($data['to'])->cc($data['cc'])->bcc($data['bcc'])->send($this->build());
 
         return $this->returnData('ok', 'email_sent_successfully');
     }
